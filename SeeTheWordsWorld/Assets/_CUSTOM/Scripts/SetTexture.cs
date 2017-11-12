799677aea12a6a2ba99806dfeba587d4a7e30044
xq8908@gmail.com <xq8908@gmail.com>
2017-11-11 23:47:04 -0500

    ReliefSilhouette

---
+++ SeeTheWordsWorld/Assets/_CUSTOM/Scripts/SetTexture.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetTexture : MonoBehaviour 
+{
+    [SerializeField]
+    Texture2D targetTex;
+
+	void Update () {
+        AddToTex(Random.Range(0, 255), Random.Range(0, 255));
+	}
+
+    void AddToTex(float x, float y)
+    {
+		targetTex.SetPixel((int)Mathf.Round(x), (int)Mathf.Round(y), Color.gray);
+
+		//Texture2D texture = new Texture2D(128, 128);
+		//GetComponent<Renderer>().material.mainTexture = texture;
+
+		//for (int i = 0; i < texture.height; i++)
+		//{
+		//	for (int j = 0; j < texture.width; j++)
+		//	{
+		//		Color color = ((j & i) != 0 ? Color.white : Color.gray);
+		//		texture.SetPixel(x, y, color);
+		//	}
+		//}
+        targetTex.Apply();
+    }
+}
